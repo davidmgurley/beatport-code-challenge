766b3631126c141a59703e43faa32ca3bccd631b
David <davidmgurley@gmail.com>
2020-07-09 09:30:29 -0600

    resize first pass

--- javascript/tests/03_resize/resize.js
+++ javascript/tests/03_resize/resize.js
@@ -5,6 +5,31 @@ const initResize = () => {
      * vertically.
      */
     const resize = document.getElementById('resize');
+    const panel = document.getElementById('panel')
+
+    var startHeight, endPosition, startPosition, positionDifference
+
+    resize.addEventListener('mousedown', initDrag)
+
+    function initDrag (e) {
+        startHeight = parseInt(document.defaultView.getComputedStyle(panel).height)
+        startPosition = e.clientY
+        document.documentElement.addEventListener('mousemove', dragElement)
+        document.documentElement.addEventListener('mouseup', endDrag)
+    }
+
+    function dragElement (e) {
+        endPosition = e.clientY
+        panel.style.backgroundColor = 'rgb(' + endPosition + ', ' + Math.abs(positionDifference) + ', 0)'
+        positionDifference = startPosition - endPosition
+        panel.style.height = startHeight + positionDifference
+    }
+
+    function endDrag () {
+        document.documentElement.removeEventListener('mousemove', dragElement)
+        document.documentElement.removeEventListener('mouseup', endDrag)
+
+    }
 };
 
 window.initResize = initResize;
